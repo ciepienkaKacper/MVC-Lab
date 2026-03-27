38c6fb55268060de7104adae7372dd7fae0f07b7
Kacper Ciepienka <kacper.ciepienka@gmail.com>
2026-03-27 14:58:54 +0100

    Lab 5 Dotnet page 1 - 6

---
+++ Dotnet/MvcMovie/Data/MvcTextContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcFilm.Data
+{
+    public class MvcTextContext : DbContext
+    {
+        public MvcTextContext (DbContextOptions<MvcTextContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
--- Dotnet/MvcMovie/Program.cs
+++ Dotnet/MvcMovie/Program.cs
@@ -1,9 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcFilm.Data;
+using MvcMovie.Models;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MvcTextContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MvcTextContext") ?? throw new InvalidOperationException("Connection string 'MvcTextContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
